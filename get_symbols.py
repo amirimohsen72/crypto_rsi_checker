33ae760bf762b36a5430a02daca2fa32f53436f7
mohsen <amirimohsen72@gmail.com>
2025-09-23 18:25:52 +0330

    #8 get all symbol

---
+++ get_symbols.py
@@ -0,0 +1,29 @@
+import time
+import ccxt
+import os
+
+
+exchange = ccxt.bybit({
+    'options': {'defaultType': 'future'}
+})
+markets = exchange.load_markets()
+
+symbols = [
+    f"{symbol}:USDT" for symbol, data in markets.items()
+    if data.get("type") == "swap" and data.get("quote") == "USDT"
+]
+
+def clear_console():
+    """Clears the console screen."""
+    # For Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # For macOS and Linux
+    else:
+        os.system('clear')
+
+
+
+with open("allsymbols.txt", "w") as f:
+    for symbol in symbols:
+        f.write(symbol + "\n")
